744a314a383ba43f76004335d05454e5505458ff
marco di candia <mdicandia89@gmail.com>
2023-03-06 18:15:32 +0100

    s

--- resources/views/article/category.blade.php
+++ resources/views/article/category.blade.php
@@ -9,14 +9,14 @@
             </div>
         </div>
 
-        <div class="container-fluid my-3 font pb-2">
+        <div class="container-fluid my-3 font pb-2 ">
             <div class="row justify-content-center">
                 @foreach ($articles as $article )
                 <div class="col-12 col-md-8 col-lg-3 my-5 justify-content-center d-flex">
                     <div class=" card-wrap" style="width:18rem">
                         <img src="{{Storage::url($article->img)}}" height="" class="img-size">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$article->title}}</h5>
+                        <div class="card-body ">
+                            <h5 class="card-title ">{{$article->title}}</h5>
                             <p class="card-text">{{$article->subtitle}}</p>
                             <div class="card-footer text-center">
                                 <p class="card-text"> redatto il {{$article->created_at->format('d/m/Y')}} da <a class="href-color" href="{{route('user.article', ['user' => $article->user->id])}}"> {{ $article->user->name}}</a></p>
--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,13 +1,15 @@
 <x-layout>
     <body class=" back-show">
         <div class="container my-2 test-show">
-            <div class="row my-5 ">
-                <div class="col-5 ">
-                    <img src="{{Storage::url($article->img)}}" class="img-fluid box-radius " height="220vh" width="280vh">
+            <div class="row my-5 justify-content-center ">
+                <div class="col-12 text-center">
+                    <img src="{{Storage::url($article->img)}}" class="img-fluid box-radius " height="320vh" width="380vh">
                 </div>
-                <div class="col-12 col-md-5 text-center">
-                    <h2 class="my-4">{{$article->title}}</h2>
-                    <p class="card-text pt-3">{{$article->body}}</p>
+                <div class="col-12 col-md-12 text-white">
+                    <h2 class="my-4 text-center">{{$article->title}}</h2>
+                    <div class="">
+                        <p class="card-text pt-3 text-size">{{$article->body}}</p>
+                    </div>
                     <div class="my-0">
                         <p>Redatto da {{$article->user->name}} il {{$article->created_at->format('d/m/Y')}}</p>
 
