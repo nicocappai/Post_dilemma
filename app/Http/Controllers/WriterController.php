9b578ea006c76541243f8be639f41ddf7f065863
Gabriele Garofalo <gabri.garo@outlook.it>
2023-03-14 13:43:55 +0100

    use story6

--- app/Http/Controllers/WriterController.php
+++ app/Http/Controllers/WriterController.php
@@ -10,9 +10,9 @@ class WriterController extends Controller
 {
     public function dashboard(){
 
-        $acceptedArticles =Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , true)->oderBy('created_at' , 'desc')->get();
-        $rejectedArticles =Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , false)->oderBy('created_at' , 'desc')->get();
-        $unrevisionedArticles = Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , NULL)->oderBy('created_at' , 'desc')->get();
+        $acceptedArticles =Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , true)->orderBy('created_at' , 'desc')->get();
+        $rejectedArticles =Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , false)->orderBy('created_at' , 'desc')->get();
+        $unrevisionedArticles = Article::where('user_id' ,Auth::user()->id)->where('is_accepted' , NULL)->orderBy('created_at' , 'desc')->get();
 
         return view('writer.dashboard' , compact('unrevisionedArticles' , 'acceptedArticles' , 'rejectedArticles'));
     }
--- resources/views/components/writer-articles-table.blade.php
+++ resources/views/components/writer-articles-table.blade.php
@@ -24,7 +24,7 @@
             </td>
             <td>{{$article->created_at->format('d/m/Y')}}</td>
             <td>
-                <a href="{{route('article.edit'), compact('article')}}" class="btn btn-warning text-white">Modifica l' articolo</a>
+                <a href="{{route('article.edit', compact('article'))}}" class="btn btn-warning text-white">Modifica l' articolo</a>
                 <a href="" class="btn card-btn">Modifica l' articolo</a>
                 <form action="" method="" class="d-inline">
                     @csrf
