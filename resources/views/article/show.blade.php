1f042002564bf677e4dcb346e565bab158f2c280
Gabriele Garofalo <gabri.garo@outlook.it>
2023-03-15 17:04:15 +0100

    fine use story7

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -8,25 +8,36 @@
                 </div>
                 <div class="col-6 col-md-3 col-lg-3 flex-column d-flex align-items-center justify-content-center">
                     <h2 class="my-4 text-center">{{substr($article->title, 0, 20)}}</h2>
-                    <h5 class="my-4 text-center">{{substr($article->subtitle, 0, 30)}}</h5>
+                    <h5 class="my-4 text-center">{{substr($article->subtitle, 0, 55)}}</h5>
                     <p class="card-text text-size">Redatto da {{$article->user->name}} il {{$article->created_at->format('d/m/Y')}}</p>
                 </div>
                 <div class="col-12  my-5 p-5 ">
                     <p class="card-text pt-3 text-size text-start" id="article-body">{{$article->body}}</p>
                 </div>
-                <div class="col-12 col-md-8 col-lg-6 d-flex justify-content-center">
-                @if (Auth::user() && Auth::user()->is_revisor && $article->is_accepted == 0)
-                    <a href="{{route('revisor.acceptArticle' , compact('article'))}}" class="btn card-btn text-white my-4 mx-2">Accetta articolo</a>
-                    <a href="{{route('revisor.rejectArticle' , compact('article'))}}" class="btn card-btn text-white my-4 mx-2">Rifiuta articolo</a>
-                @endif
-                    <a href="{{URL::previous()}}" class="btn card-btn text-white mx-2 my-4 text-center">Torna indietro</a>
+                <div class="row">
+
+                        @if (Auth::user() && Auth::user()->is_revisor && $article->is_accepted == 0)
+                        <div class="col-12 col-md-4 col-lg-4 d-flex justify-content-end size-bottom">
+                            <a href="{{route('revisor.acceptArticle' , compact('article'))}}" class="btn card-btn text-white my-4 mx-2 ">Accetta articolo</a>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-4 d-flex justify-content-center size-bottom">
+                            <a href="{{route('revisor.rejectArticle' , compact('article'))}}" class="btn card-btn text-white my-4  mx-2">Rifiuta articolo</a>
+                        </div>
+
+                        @endif
+                        <div class="col-12 col-md-4 col-lg-4 d-flex justify-content-start size-bottom">
+                            <a href="{{URL::previous()}}" class="btn card-btn text-white mx-2 my-4 text-center">Torna indietro</a>
+                        </div>
+
+
                 </div>
+
             </div>
         </div>
     </body>
     <a href="#inizio">
         <div id="tornasu">
-            <img src= "/images/icon-top.png" class="tornasu" width="60px" height="60px">  
+            <img src= "/images/icon-top.png" class="tornasu" width="60px" height="60px">
         </div>
     </a>
 </x-layout>
